4f8d3ecc29695c13377d54c8086241fc70a6963f
Sergey <example>
2019-12-09 10:57:22 +0200

    first commit

---
+++ title.php
@@ -0,0 +1,8 @@
+				<ul class="top-nav_menu">
+  	                 	<?php echo ("<li><a href=index.php?id=0>Главная</a></li>"); ?> 
+  	                    <li><a href="about.php">О компании</a></li>
+  	                    <li><a href="1.php">Наши услуги</a></li></a></li>
+  	                    <li><a href="ipoteka.php">Ипотека</a></li>
+                        <li><a href="invest.php">Инвесторам</a></li>
+  	  	                <li><a href="contacts.php">Контакты</a></li>
+  	                 </ul>
